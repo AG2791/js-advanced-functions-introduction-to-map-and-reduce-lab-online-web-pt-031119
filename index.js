8b3c9a20b02ff9ddc5b7fabff31818cce4fb578d
AJ Guiles <ashiguiles@gmail.com>
2019-11-09 04:56:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,3 +34,25 @@ function mapToSquare(sourceArray) {
 }
 
 
+
+function reduceToTotal(sourceArray, startingPoint=0) {
+  let total = startingPoint
+  for (let i = 0; i < sourceArray.length; i++ ) {
+    total = total + sourceArray[i]
+  }
+  return total
+}
+
+function reduceToAllTrue(src) {
+  for (let i = 0; i < src.length; i++ ) {
+    if (!src[i]) return false
+  }
+  return true
+}
+
+function reduceToAnyTrue(src) {
+  for (let i = 0; i < src.length; i++ ) {
+    if (src[i]) return true
+  }
+  return false
+}
