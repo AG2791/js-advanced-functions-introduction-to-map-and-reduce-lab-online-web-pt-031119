b6f88781b7660814106cb71f0b5176fdcf9b7164
AJ Guiles <ashiguiles@gmail.com>
2019-11-09 04:39:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Your code here
 function mapToNegativize(sourceArray) {
   let array = []
-  for (let i = 0; i < src.length; i++ ) {
+  for (let i = 0; i < sourceArray.length; i++ ) {
     array.push(-1 * sourceArray[i])
   }
   return array
