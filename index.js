1defbf24d519eafa1fead99e0037ce44a233babb
AJ Guiles <ashiguiles@gmail.com>
2019-11-09 04:54:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 ///Your code here
 
-// function mapToNegativize(sourceArray) {
-//   let array = []
-//   for (let i = 0; i < sourceArray.length; i++ ) {
-//     array.push(-1 * sourceArray[i])
-//   }
-//   return array
-// }
-
-function mapToNegativize(src) {
-  let r = []
-  for (let i = 0; i < src.length; i++ ) {
-    r.push(-1 * src[i])
+function mapToNegativize(sourceArray) {
+  let array = []
+  for (let i = 0; i < sourceArray.length; i++ ) {
+    array.push(-1 * sourceArray[i])
   }
-  return r
+  return array
 }
 
 
