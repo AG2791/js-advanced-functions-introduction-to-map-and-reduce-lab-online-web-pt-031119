018cdf469500a8e81a808d57a1786dd2536d9036
AJ Guiles <ashiguiles@gmail.com>
2019-11-09 04:38:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 function mapToNegativize(sourceArray) {
   let array = []
   for (let i = 0; i < src.length; i++ ) {
-    array.push(-1 * src[i])
+    array.push(-1 * sourceArray[i])
   }
   return array
 }
 
-function mapToNegativize(src) {
-  let r = []
-  for (let i = 0; i < src.length; i++ ) {
-    r.push(-1 * src[i])
-  }
-  return r
-}
+// function mapToNegativize(src) {
+//   let r = []
+//   for (let i = 0; i < src.length; i++ ) {
+//     r.push(-1 * src[i])
+//   }
+//   return r
+// }
