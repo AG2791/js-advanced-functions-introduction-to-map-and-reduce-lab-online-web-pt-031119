192069a68424a0aa982378babba1bf263be7ee58
AJ Guiles <ashiguiles@gmail.com>
2019-11-09 04:45:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function mapToNegativize(sourceArray) {
 function mapToNoChange(sourceArray) {
   let array = []
   for (let i = 0; i < sourceArray.length; i++) {
-    array.push([1])
+     array.push(sourceArray[i])
   }
 }
